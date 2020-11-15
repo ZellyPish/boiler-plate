b6e8e522c87672f198062d939e08cd7ea999ad67
JC K <ehxhfl21@gmail.com>
2020-11-15 21:53:06 +0900

    클라이언트 추가, 서버단 분리

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://zelly:qrwe1423@boilerplate.fgowv.mongodb.net/test?retryWrites=true&w=majority",
+};
